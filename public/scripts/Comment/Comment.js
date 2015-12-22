6740795bf53da732f92638633442d23eb05be2d2
muhammadfaizan <muhammad.faizan01@live.com>
2015-12-22 17:54:05 +0500

    tutorial 10

--- public/scripts/Comment/Comment.js
+++ public/scripts/Comment/Comment.js
@@ -1,55 +1,59 @@
 var data = [
-  {id: 1, author: "Pete Hunt", text: "This is one comment"},
-  {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
+    {
+        id: 1,
+        author: "Muhammad Faizan",
+        text: "This is one comment"
+    },
+    {
+        id: 2,
+        author: "Jordan Walke",
+        text: "This is *another* comment"
+    }
 ];
 
 var Comment = React.createClass({
-    rawMarkup: function() {
+    rawMarkup: function () {
         var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
-        return {__html: rawMarkup};
+        return {
+            __html: rawMarkup
+        };
     },
-    render: function() {
-        return (
-            <div className="comment">
-                <h2 className="commentAuthor">
-                    {this.props.author}
-                </h2>
-                <span dangerouslySetInnerHTML={this.rawMarkup()} />
-            </div>
+    render: function () {
+        return ( < div className = "comment" >
+            < h2 className = "commentAuthor" > {this.props.author} </h2> 
+                < span dangerouslySetInnerHTML = {this.rawMarkup()}
+            /></div>
         )
     }
 })
 
 
-var CommentList = React.createClass ({
-    displayName:'CommentBox',
-   render: function() {
-       return (
-           <div className = "commentList">
-                <Comment author="Pete Hunt"> This is one comment</Comment>
-           <Comment author="Jordan Walke">This is *another* comment</Comment>
-            </div>
-       );
-   } 
+var CommentList = React.createClass({
+    displayName: 'CommentBox',
+    render: function () {
+        var commentNodes = this.props.data.map(function (comment) {
+            return ( < Comment author = {comment.author}
+                key = {comment.id} >{comment.text}</Comment>
+            );
+        });
+        return ( <div className = "commentList" > {commentNodes} </div>
+               );
+    }
 });
 
-var CommentForm = React.createClass ({
-   render: function() {
-       return (
-           <div className = "commentForm">
-                Hello, world! I am a CommentForm.
-            </div>
-       );
-   } 
+var CommentForm = React.createClass({
+    render: function () {
+        return ( < div className = "commentForm" >
+            Hello, world!I am a CommentForm. < /div>
+        );
+    }
 });
-var CommentBox =React.createClass ({
-    displayName:'CommentBox',
-    render: function(){
-        return (
-            <div className= "commentBox">
-                <h1>Comments</h1>
-                <CommentList data={this.props.data}/>
-                <CommentForm />
+var CommentBox = React.createClass({
+    displayName: 'CommentBox',
+    render: function () {
+        return ( < div className = "commentBox" >
+            < h1 > Comments < /h1>
+                < CommentList data={this.props.data}/> <CommentForm />
             </div>
         );
     }
@@ -57,7 +61,7 @@ var CommentBox =React.createClass ({
 
 
 
-ReactDOM.render(
-    <CommentBox data={data} />,
+ReactDOM.render( < CommentBox data = {data}
+    />,
     document.getElementById('content')
 )
