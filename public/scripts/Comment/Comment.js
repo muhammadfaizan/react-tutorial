c890d7821ccfd36cc607bc9b2929d6062669339b
muhammadfaizan <muhammad.faizan01@live.com>
2015-12-23 10:29:56 +0500

    TUtorial 15

--- public/scripts/Comment/Comment.js
+++ public/scripts/Comment/Comment.js
@@ -45,8 +45,12 @@ var CommentList = React.createClass({
 
 var CommentForm = React.createClass({
     render: function () {
-        return ( < div className = "commentForm" >
-            Hello, world!I am a CommentForm. < /div>
+        return ( 
+            <form className = "commentForm" >
+                <input type="text" placeholder="Your name" />
+                <input type="text" placeholder="Say something..."/>
+                <input type="submit" value="Post" />
+            </form>
         );
     }
 });
